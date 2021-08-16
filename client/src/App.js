da6bf3576c02243edc92ec49a3aab78043794b59
David01051 <76750253+David01051@users.noreply.github.com>
2021-08-16 14:40:44 +0300

    this is david

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+// this is david
 function App() {
   
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          david <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
