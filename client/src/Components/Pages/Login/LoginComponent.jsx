d0dc131c71ec571304dd3e80de48e604a209c7bd
David01051 <76750253+David01051@users.noreply.github.com>
2021-08-19 18:44:27 +0300

    design change login and footer

--- client/src/Components/Pages/Login/LoginComponent.jsx
+++ client/src/Components/Pages/Login/LoginComponent.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Login = ()=> {
     return (
-        <div>
-            Login
+        <div className="login">
+            <div className="login-user">
+                login
+
+            </div>
+            
         </div>
     )
 }
