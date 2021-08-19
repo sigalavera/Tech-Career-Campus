c7c7baf49441d0bbcc4f5849f534e56dfaa2af84
SHUANESH <shuanesh1817@gmail.com>
2021-08-19 10:41:30 +0300

    add get

--- server/controller/courseController.js
+++ server/controller/courseController.js
@@ -12,8 +12,20 @@ const addNewCourse = async (req, res) => {
   }
 };
 
+const getCourse = async (req, res) => {
+  try {
+    await CourseModel.find({}, (err, result) => {
+      if (err) console.log(err); 
+      res.status(200).json({ massage: "get course success!", data: result })
+
+    });
+  } catch (err) {
+    res.status(500).json({ massage:  "get course field", error: err });
+  }
+};
 
 module.exports = {
      addNewCourse ,
+     getCourse,
 
 };
--- server/route/courseRouting.js
+++ server/route/courseRouting.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const courseController = require("../controller/courseController")
 
 router.post('/', courseController.addNewCourse);
+router.get('/', courseController.getCourse);
 
 module.exports = router;
