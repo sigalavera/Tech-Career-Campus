9b7e8f3c5e88509126517f690f7ef782b2237e1c
David01051 <76750253+David01051@users.noreply.github.com>
2021-08-20 22:15:16 +0300

    styleing media query

--- client/src/Components/Pages/Login/LoginComponent.jsx
+++ client/src/Components/Pages/Login/LoginComponent.jsx
@@ -1,15 +1,23 @@
+
 import React from 'react'
 import "./Login.css";
 
-const Login = ()=> {
-    return (
-        <div className="login">
-            <div className="login-user">
-                login
+import React from "react";
+ 
 
-            </div>
-            
-        </div>
-    )
-}
-export default Login
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="login-user">
+        <form>
+          <input type="text" placeholder="Email Adress" />
+          <br />
+          <input type="password" placeholder="Password" />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default Login;
--- server/controller/studentController.js
+++ server/controller/studentController.js
@@ -3,29 +3,41 @@ const StudentModel = require('../models/studentModel')
 const addNewStudent = async (req, res) => {
   try {
     await StudentModel.insertMany([req.body], (err, result) => {
-      if (err) console.log(err); 
-      // const { password, ...studentInfo } = result;
+      if (err) console.log(err);
       res.status(200).json({ massage: "create Student success!", data: result })
 
     });
   } catch (err) {
-    res.status(500).json({ massage:  "creat new Student field", error: err });
+    res.status(500).json({ massage: "creat new Student field", error: err });
   }
 };
 
 const getStudent = async (req, res) => {
   try {
     await StudentModel.find({}, (err, result) => {
-      if (err) console.log(err); 
+      if (err) console.log(err);
       res.status(200).json({ massage: "get Student success!", data: result })
 
     });
   } catch (err) {
-    res.status(500).json({ massage:  "get Student field", error: err });
+    res.status(500).json({ massage: "get Student field", error: err });
   }
 };
 
+const GetStudentGradeById = async (req, res) => {
+  try {
+    StudentModel.findOne({ IdNumber: req.body.IdNumber }, (error, result) => {
+      if (error) throw error
+      res.status(200).json({ massage: "get Student grades by id success!", data: result.tests })
+
+    })
+  }
+  catch (err) {
+    res.status(500).json({ massage: "get Student grades by id", error: err });
+  }
+}
 module.exports = {
   addNewStudent,
-  getStudent
+  getStudent,
+  GetStudentGradeById
 };
