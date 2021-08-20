77084be93ad208377e8bf8a6dc72ad3228ce6a84
itzhackB <yzikbynesai@gmail.com>
2021-08-20 18:35:03 +0300

    add syllabus list

--- client/src/Components/Pages/Syllabus/SyllabusComponent.jsx
+++ client/src/Components/Pages/Syllabus/SyllabusComponent.jsx
@@ -1,4 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { fetchSyllabus } from '../../../FetchFunctions/FetchFunctions'
+
+
 const SyllabusComponent = () => {
-  return <h1>סילבוס</h1>;
+  const [syllabusData, setSyllabusData] = useState()
+
+  useEffect(() => {
+
+    fetchSyllabus()
+      .then(data => {
+        setSyllabusData(data.data[0]);
+      })
+
+  }, [])
+ 
+  return (
+    <>
+      <h1>סילבוס</h1>
+      <VerticalTimeline>
+        <h1>{syllabusData?.name}</h1>
+        {syllabusData?.CourseInformation.map((courseItem => {
+          return (
+            <VerticalTimelineElement
+            key={courseItem.nameSubject}
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+              contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+              date="2011 - present"
+              iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+              icon
+            >
+              <h2 className="vertical-timeline-element-title">{courseItem.nameSubject}</h2>
+              <h4 className="vertical-timeline-element-subtitle">
+                <ul>
+                  {courseItem.topics.map((topic) => {
+                    return (
+                      <li>
+                        {topic.subject}
+                      </li>
+                    )
+                  })}
+                </ul>
+              </h4>
+              <p>
+                {courseItem.summery}
+              </p>
+            </VerticalTimelineElement>)
+        }))}
+
+      </VerticalTimeline>;
+    </>
+  )
 };
 export default SyllabusComponent;
