b10fad22d2f1401bdf31d2f1cb5390a802c8fb2b
NoyCoca <noycoca123@gmail.com>
2021-08-21 20:07:50 +0300

    create grade componnet for admin and student

--- client/src/Components/Pages/CourseSchedule/CourseScheduleComponent.jsx
+++ client/src/Components/Pages/CourseSchedule/CourseScheduleComponent.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 const CourseSchedule = ()=>{
     return (
-        <div>
-      <iframe src="https://calendar.google.com/calendar/embed?src=104sovb0e3cdrk1rplkap2b91g%40group.calendar.google.com&ctz=Asia%2FJerusalem" style={{border:" 0"}} fontSize="12" width="300" height="300" frameborder="1" scrolling="yes"></iframe>
-        </div>
-    )
+      <div>
+        <iframe
+          title={"Course schedule"}
+          src="https://calendar.google.com/calendar/embed?src=104sovb0e3cdrk1rplkap2b91g%40group.calendar.google.com&ctz=Asia%2FJerusalem"
+          style={{ border: " 0" }}
+          fontSize="12"
+          width="300"
+          height="300"
+          frameBorder="1"
+          scrolling="yes"
+        ></iframe>
+      </div>
+    );
 }
 export default CourseSchedule
--- client/src/Routing/MyCourseRouting.js
+++ client/src/Routing/MyCourseRouting.js
@@ -3,16 +3,21 @@ import {
     Route,
     Switch
 } from "react-router-dom";
+import CourseSchedule from '../Components/Pages/CourseSchedule/CourseScheduleComponent';
 
 import DailySchedule from '../Components/Pages/DailySchedule/DailyScheduleComponent';
-import Grades from '../Components/Pages/Grades/GradesComponent'
+import StudentGradesComponent from '../Components/Pages/Grades/StudentGradesComponent'
+import AdminGradesComponent from '../Components/Pages/Grades/AdminGradesComponent'
 import Syllabus from '../Components/Pages/Syllabus/SyllabusComponent'
 
 const MyCourseRouting = () => {
+    // temp role for switch component
+    const role = "Admin"
     return (
         <Switch>
             <Route path="/my-course/daily-schedule" component={DailySchedule} />
-            <Route path="/my-course/grades" component={Grades} />
+            <Route path="/my-course/grades" component={role === 'Student' ? StudentGradesComponent : AdminGradesComponent} />
+            <Route path="/my-course/course-schedule" component={CourseSchedule} />
             <Route path="/my-course/syllabus" component={Syllabus} />
         </Switch>
     )
