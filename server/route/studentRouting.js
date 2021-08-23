e15018935577aa75096c666879e4734da1504360
itzhak12380 <iascc12380@gmail.com>
2021-08-23 22:52:32 +0300

    changed student

--- server/route/studentRouting.js
+++ server/route/studentRouting.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const studentController = require("../controller/studentController")
 
-router.post('/', studentController.addNewStudent);
+router.post('/:id', studentController.addNewStudent);
 router.get('/', studentController.getStudent);
 router.get('/gradesById', studentController.getStudentGradeById);
 router.post('/addTestById', studentController.addStudentTestById);
-router.post('/updateTest/:_id', studentController.updateStudentTestById);
+router.put('/updateTest/:_id', studentController.updateStudentTestById);
 router.delete('/deleteTest/:_id', studentController.deleteStudentTestById);
 module.exports = router;
