7131b7215fdab5fe64b2b0f8f740c81161fb7ef1
NoyCoca <noycoca123@gmail.com>
2021-08-24 23:53:44 +0300

    fix conflicts

--- server/route/studentRouting.js
+++ server/route/studentRouting.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {isToken} = require("../controller/authorization/isToken")
+const isToken = require("../controller//authorization/isToken")
 const studentController = require("../controller/studentController")
 
 router.post('/', studentController.addNewStudent);
-router.get('/',isToken, studentController.getStudent);
+router.get('/', isToken, studentController.getStudent);
 router.get('/gradesById', studentController.getStudentGradeById);
 router.post('/addTestById', studentController.addStudentTestById);
 router.put('/updateTest/:_id', studentController.updateStudentTestById);
