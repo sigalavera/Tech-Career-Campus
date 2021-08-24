7ac0bae48e29c50d8adb5840ca40083468f75863
SHUANESH <shuanesh1817@gmail.com>
2021-08-24 09:16:19 +0300

    isToken-fixsd

--- server/controller/authorization/isToken.js
+++ server/controller/authorization/isToken.js
@@ -2,28 +2,30 @@ const jwt = require("jsonwebtoken");
 
 const isToken = (req, res, next) => {
   const header = req.header("Authorization");
-  console.log(header);
   const [bearer, token] = header.split(" ");
 
   if (bearer === "Bearer" && typeof token !== "undefined") {
     try {
       let payload = jwt.verify(token, process.env.SECRET_KEY);
-      res.status(200).json({ massage: "get the token", date: payload });
+      if (req.body.id !== payload._id) {
+        throw Error.name;
+      } else {
+        next();
+      }
     } catch (error) {
       res.status(401).json({ massage: "invalid or expired token", error });
     }
-  } else {
-    res.status(401).json({ massage: "invalid token", error });
   }
-next();
 };
 
 const autoRole = (Role) => {
   return (req, res, next) => {
-    if (req.body.role !== Role) res.status(401);
-    return res.sand("not allowed");
+    if (req.body.role !== Role) {
+      throw Error.name;
+    } else {
+      next();
+    }
   };
-  next();
 };
 
 module.exports = {
