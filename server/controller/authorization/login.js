f2518ec2854500c4b1b0221990c4122db21a835c
NoyCoca <76784742+NoyCoca@users.noreply.github.com>
2021-08-26 16:04:20 +0300

    Update login.js

--- server/controller/authorization/login.js
+++ server/controller/authorization/login.js
@@ -5,47 +5,6 @@ const bcrypt = require("bcrypt");
 const validateLoginInput = require("./loginValidator");
 const SECRET_KEY = process.env.SECRET_KEY
 const login = async (req, res) => {
-<<<<<<< HEAD
- 
-    if(req.body.role === "Staff"){
-        const {errors, isValid } = validateLoginInput(req.body);
-        if (!isValid)
-        return res.status(404).json({ message: "staff doesn't exist.", err:errors });
-  
-        const { email, password } = req.body;
-        try {
-          const staff = await StaffModel.findOne({ email });
-
-          const isPasswordCorrect = await bcrypt.compare(password, staff.password);
-          if (!isPasswordCorrect)
-            res.status(400).json({ message: "Invalid credentials" });
-
-
-          let payload =
-          { 
-              id: staff._id,  
-              email: staff.email,
-              firstName:staff.firstName,
-              lastName:staff.lastName,
-              role:staff.role,
-              phone:staff.phone,
-              age:staff.age,
-              profileImg:staff.profileImg,
-              IdNumber:staff.IdNumber
-
-          }
-          const token = jwt.sign(
-            payload,
-            SECRET_KEY,
-            { expiresIn: "1d" }
-          );
-        
-          res.status(200).json({message:"success", result: token });
-        } catch (err) {
-          res.status(500).json({ message: "something went wrong", err:errors });
-        }
-=======
->>>>>>> 93760a17d8b0d4eeb7745ce66b1bd9a2705aa12b
 
   if (req.body.role === "Staff") {
     const { errors, isValid } = validateLoginInput(req.body);
@@ -61,10 +20,10 @@ const login = async (req, res) => {
       }
 
       const isPasswordCorrect = await bcrypt.compare(password, staff.password)
-      
-      
+
+
       if (!isPasswordCorrect)
-        res.status(400).json({ errors: { password: "wrong password"} });
+        res.status(400).json({ errors: { password: "wrong password" } });
 
 
       let payload =
