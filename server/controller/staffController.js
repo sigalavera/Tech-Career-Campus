baf1a13e1266cb3017e5810d9b2c390106f5354d
CHEN ZAUDA <czauda@gmail.com>
2021-08-31 19:00:22 +0300

    role to function delete on furom

--- server/controller/staffController.js
+++ server/controller/staffController.js
@@ -1,5 +1,4 @@
 const StaffModel = require('../models/staffModel');
-const StudentModel = require('../models/studentModel')
 
 const addNewStaff = async (req, res) => {
   try {
--- server/route/forumRouting.js
+++ server/route/forumRouting.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const messagesController = require("../controller/forumController");
+const {authRole} = require('../controller/authentication/auth');
 
 router.post("/staff", messagesController.messagesByStaff);
 router.post("/student", messagesController.messagesByStudent);
 router.get("/", messagesController.getAllMessages);
-router.delete("/:id", messagesController.deleteMessage);
+router.delete("/:id", authRole("Staff"), messagesController.deleteMessage);
 
 module.exports = router;
