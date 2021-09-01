3e4f8559fbaf6ba7b3570f079f4c3e0e9aa81bc8
SlumpG <shlomo.gangul1@gmail.com>
2021-09-01 23:28:29 +0300

    added news ticker

--- client/src/Components/Pages/News/NewsComponent.jsx
+++ client/src/Components/Pages/News/NewsComponent.jsx
@@ -1,19 +1,35 @@
 import React, { useState, useEffect } from "react";
 import fetcher from "../../../utils/fetcher";
+import Ticker from 'react-ticker'
 
 const News = () => {
   const [newsData, setNewsData] = useState([]);
 
-  const newsApi = process.env.REACT_APP_NEWS_API
-  const newsApiKey = process.env.REACT_APP_NEWS_API_KEY
+  const newsApi = process.env.REACT_APP_NEWS_API;
+  const newsApiKey = process.env.REACT_APP_NEWS_API_KEY;
 
   useEffect(() => {
-    fetcher(`${newsApi}${newsApiKey}`)
-      .then((response) => setNewsData(response.articles));
+    fetcher(`${newsApi}${newsApiKey}`).then((response) =>
+      setNewsData(response.articles)
+    );
   }, []);
 
   console.log(newsData);
 
-  return <div></div>;
+  return (
+<div className="news-ticker-div">
+      <Ticker  mode="smooth" direction='toRight'>
+        {({ index }) => (
+          <>
+            <h1>{newsData[index]?.source?.name}</h1>
+            <h1>{newsData[index]?.description}</h1>
+            <h1>{newsData[index]?.title}</h1>
+            <img src={newsData[index]?.urlToImage} alt="" />
+          </>
+        )}
+      </Ticker>
+      </div>
+
+  );
 };
 export default News;
