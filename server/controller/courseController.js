a7bd6bad57768e6841cecd9cb53d8be407c24ade
itzhak12380 <iascc12380@gmail.com>
2021-09-08 16:06:39 +0300

    added function to update subjects fields

--- server/controller/courseController.js
+++ server/controller/courseController.js
@@ -186,6 +186,40 @@ const updateSubSubject = async (req, res) => {
   }
 
 };
+const updateSubject = async (req, res) => {
+  try {
+    const field = await req.body.field
+    const SubjectPath = `CourseInformation.$[objcet].${field}`
+    const SubjectField = {}
+    SubjectField[SubjectPath] = req.body.newValue
+    await CourseModel.findOneAndUpdate(
+     { _id: req.body._id},
+      { $set:  SubjectField},
+      {
+        arrayFilters: [{ "objcet._id": { _id: req.body.Subject_id } }],
+        upsert: true
+      },
+      (err, result) => {
+        if (err) throw err;
+
+        if (result !== null) {
+          res
+            .status(200)
+            .json({ message: "update corse subject was success!", data: result });
+        } else {
+          const errorNull = new Error("result is null");
+          res
+            .status(500)
+            .json({ message: "update course subject field", error: errorNull.message });
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "update course subject field", error: err });
+  }
+
+};
 
 module.exports = {
   addNewCourse,
@@ -193,5 +227,6 @@ module.exports = {
   getCourseByName,
   deleteSubSubject,
   addSubSubject,
-  updateSubSubject
+  updateSubSubject,
+  updateSubject
 };
--- server/route/courseRouting.js
+++ server/route/courseRouting.js
@@ -9,6 +9,7 @@ router.post('/getCourseByName', courseController.getCourseByName);
 router.delete('/deleteSubSubject', authRole("Staff"), courseController.deleteSubSubject);
 router.post('/addSubSubject', authRole("Staff"), courseController.addSubSubject);
 router.put('/updateSubSubject', authRole("Staff"), courseController.updateSubSubject);
+router.put('/updateSubject', authRole("Staff"), courseController.updateSubject);
 
 
 module.exports = router;
